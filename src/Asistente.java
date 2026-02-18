91546503de5f356b6a0dc163bb0a125884e4f302
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 10:26:20 +0100

    Update Asistente.java

--- src/Asistente.java
+++ src/Asistente.java
@@ -1,64 +1,49 @@
-import java.util.*;
+public class Asistente {
+    //objeto asistente con nombre, apellido y mail
+    private String nombre;
+    private String apellido;
+    private String mail;
 
-public class Main{
-    public static void main (String[] args){
-        Scanner sc = new Scanner(System.in);
-        Set<Asistente> asistentes = new HashSet<>();
-        boolean salir = false;
+    //constructor
+    public Asistente(String nombre, String apellido, String mail) {
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.mail = mail;
+    }
 
+    //getter y setters
 
-        //bucles de flujo
-        while(!salir){
-            System.out.println("Elige una opcion");
-            System.out.println("1. Registrar nuevo asistente");
-            System.out.println("2. Buscar un asistente por su mail");
-            System.out.println("3. Mostrar numero de asistentes");
-            System.out.println("4. Mostrar los mails de los asistentes");
-            System.out.println("5. Salir");
-            int opcion = sc.nextInt();
-            sc.nextLine();
+    public String getNombre() {
+        return nombre;
+    }
 
-            switch (opcion){
-                case 1:
-                    System.out.println("Introduce los datos del asistente. ");
-                    System.out.println("Nombre: ");
-                    String nombre = sc.nextLine();
-                    System.out.println("Apellidos: ");
-                    String apellidos = sc.nextLine();
-                    System.out.println("Mail: ");
-                    String mail = sc.nextLine();
-                    Asistente a = new Asistente(nombre,apellidos,mail);
-                    asistentes.add(a);
-                    break;
+    public String getApellido() {
+        return apellido;
+    }
 
-                case 2:
-                    System.out.println("Introduce el mail del asistente. ");
-                    System.out.println("Mail: ");
-                     mail = sc.nextLine();
-                     for (Asistente a1 : asistentes){
-                         if (a1.getMail().equals(mail)){
-                             System.out.println("El mail del asistente ya esta registrado");
-                         }else {
-                             System.out.println("El mail del asistente  no esta registrado");
-                         }
-                     }
-                     break;
+    public String getMail() {
+        return mail;
+    }
 
-                case 3:
-                    System.out.println(asistentes.size());
-                    break;
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
 
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
 
-                case 4:
-                    for (Asistente a1:asistentes){
-                        System.out.println(a1.getMail());
-                    }break;
+    public void setMail(String mail) {
+        this.mail = mail;
+    }
 
-                case 5:
-                    salir = true;
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 
-
-            }sc.close();
-        }
+    @Override
+    protected Object clone() throws CloneNotSupportedException {
+        return super.clone();
     }
 }
