03a22ace8f19eff62ed1c972c2e1c4335f23bf66
carlosaegar <carlos.saegar@educa.jcyl.es>
2026-02-18 10:26:00 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,64 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-  //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-  // to see how IntelliJ IDEA suggests fixing it.
-  IO.println(String.format("Hello and welcome!"));
+import java.util.*;
 
-  for (int i = 1; i <= 5; i++) {
-    //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-    // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-    IO.println("i = " + i);
-  }
+public class Main{
+    public static void main (String[] args){
+        Scanner sc = new Scanner(System.in);
+        Set<Asistente> asistentes = new HashSet<>();
+        boolean salir = false;
+
+
+        //bucles de flujo
+        while(!salir){
+            System.out.println("Elige una opcion");
+            System.out.println("1. Registrar nuevo asistente");
+            System.out.println("2. Buscar un asistente por su mail");
+            System.out.println("3. Mostrar numero de asistentes");
+            System.out.println("4. Mostrar los mails de los asistentes");
+            System.out.println("5. Salir");
+            int opcion = sc.nextInt();
+            sc.nextLine();
+
+            switch (opcion){
+                case 1:
+                    System.out.println("Introduce los datos del asistente. ");
+                    System.out.println("Nombre: ");
+                    String nombre = sc.nextLine();
+                    System.out.println("Apellidos: ");
+                    String apellidos = sc.nextLine();
+                    System.out.println("Mail: ");
+                    String mail = sc.nextLine();
+                    Asistente a = new Asistente(nombre,apellidos,mail);
+                    asistentes.add(a);
+                    break;
+
+                case 2:
+                    System.out.println("Introduce el mail del asistente. ");
+                    System.out.println("Mail: ");
+                     mail = sc.nextLine();
+                     for (Asistente a1 : asistentes){
+                         if (a1.getMail().equals(mail)){
+                             System.out.println("El mail del asistente ya esta registrado");
+                         }else {
+                             System.out.println("El mail del asistente  no esta registrado");
+                         }
+                     }
+                     break;
+
+                case 3:
+                    System.out.println(asistentes.size());
+                    break;
+
+
+                case 4:
+                    for (Asistente a1:asistentes){
+                        System.out.println(a1.getMail());
+                    }break;
+
+                case 5:
+                    salir = true;
+
+
+            }sc.close();
+        }
+    }
 }
